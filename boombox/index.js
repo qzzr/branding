d26617f3f90e1329698064622c50833bf8af6ba8
mndvns <mail@mndvns.com>
2015-06-08 16:42:56 -0600

    add color values for boombox

--- boombox/index.js
+++ boombox/index.js
@@ -1,5 +1,10 @@
 exports = module.exports = require('../');
 
 exports.color = exports.color || {};
+
+exports.color.black = 'hsl(210, 24%, 13%)';
+exports.color.blue = 'hsl(220, 73%, 59%)';
+exports.color.charcoal = 'hsl(210, 17%, 25%)';
+
 exports.color.primary = exports.color.blue;
 exports.color.accent = exports.color.red;
