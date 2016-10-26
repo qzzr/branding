ed19f339b309da8f0035c4d4325d34f4b05e1b3d
Jake Richan <jake.richan@gmail.com>
2016-10-26 13:38:50 -0600

    remove unused variables

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ out.headingFontFamily = out.fontFamily;
 out.headingFontWeight = 'bold';
 out.headingLineHeight = '1.25';
 out.monospaceFontFamily = "'Source Code Pro', Menlo, Consolas, monospace",
-out.h1 = '2rem';
-out.h2 = '1.5rem';
-out.h3 = '1.25rem';
-out.h4 = '1rem';
-out.h5 = '.875rem';
-out.h6 = '.75rem';
-out.h7 = '1rem';
-out.h8 = '.875rem';
-out.h9 = '.75rem';
-out.h10 = '.625rem';
 out.boldFontWeight = 'bold';
 out.buttonFontSize = 'inherit';
 out.buttonFontWeight = 'bold';
@@ -82,58 +72,6 @@ out.shadow2 = '0 6px 10px hsla(0, 0%, 0%, 0.3), 0 2px 2px hsla(0, 0%, 0%, 0.2)';
 out.shadow3 = '0 13px 25px hsla(0, 0%, 0%, 0.3), 0 11px 7px hsla(0, 0%, 0%, 0.19)';
 out.shadow4 = '0 20px 40px hsla(0, 0%, 0%, 0.3), 0 14px 12px hsla(0, 0%, 0%, 0.17)';
 out.shadow5 = '0 27px 55px hsla(0, 0%, 0%, 0.3), 0 17px 17px hsla(0, 0%, 0%, 0.15)';
-
-// spacing
-
-out.space1 = '.25rem';
-out.space2 = '.5rem';
-out.space3 = '.75rem';
-out.space4 = '1rem';
-out.space5 = '1.25rem';
-out.space6 = '1.5rem';
-out.space7 = '1.75rem';
-out.space8 = '2rem';
-out.space9 = '2.25rem';
-out.space10 = '2.5rem';
-out.space11 = '2.75rem';
-out.space12 = '3rem';
-out.space13 = '3.25rem';
-out.space14 = '3.5rem';
-out.space15 = '3.75rem';
-out.space16 = '4rem';
-out.space17 = '4.25rem';
-out.space18 = '4.5rem';
-out.space19 = '4.75rem';
-out.space20 = '5rem';
-out.space21 = '5.25rem';
-out.space22 = '5.5rem';
-out.space23 = '5.75rem';
-out.space24 = '6rem';
-
-out.spaceScale1 = '.25';
-out.spaceScale2 = '.5';
-out.spaceScale3 = '.75';
-out.spaceScale4 = '1';
-out.spaceScale5 = '1.25';
-out.spaceScale6 = '1.5';
-out.spaceScale7 = '1.75';
-out.spaceScale8 = '2';
-out.spaceScale9 = '2.25';
-out.spaceScale10 = '2.5';
-out.spaceScale11 = '2.75';
-out.spaceScale12 = '3';
-out.spaceScale13 = '3.25';
-out.spaceScale14 = '3.5';
-out.spaceScale15 = '3.75';
-out.spaceScale16 = '4';
-out.spaceScale17 = '4.25';
-out.spaceScale18 = '4.5';
-out.spaceScale19 = '4.75';
-out.spaceScale20 = '5';
-out.spaceScale21 = '5.25';
-out.spaceScale22 = '5.5';
-out.spaceScale23 = '5.75';
-out.spaceScale24 = '6';
 
 // font sizes
 
