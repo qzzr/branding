2bb2a8c70cc9d24cec0e26ee501340cc7d013af7
Jake Richan <jake.richan@gmail.com>
2017-02-10 11:47:40 -0700

    remove enterprise, add qzzr17 with new color and root properties

--- qzzr/index.js
+++ qzzr/index.js
@@ -1,5 +1,13 @@
 exports = module.exports = require('../');
 
+exports.brandName = 'qzzr';
 exports.color = exports.color || {};
 exports.color.primary = exports.color.violet;
 exports.color.accent = exports.color.green;
+exports.color.backgroundColor = exports.color.azure;
+exports.color.textColor = exports.color.grayBlue;
+exports.color.appBarBackground = exports.color.violet;
+exports.color.appBarTextColor = exports.color.white;
+exports.color.sideNavBackground = exports.color.white;
+exports.color.dashboardBackground = exports.color.azure;
+exports.color.dashboardTextColor = exports.color.gray;
--- qzzr17/index.js
+++ qzzr17/index.js
@@ -1,11 +1,12 @@
 exports = module.exports = require('../');
 
-exports.brandName = 'boombox';
+exports.brandName = 'qzzr';
 exports.theme = 'dark';
 exports.color = exports.color || {};
 exports.color.primary = exports.color.royal;
 exports.color.accent = exports.color.royal;
 exports.color.backgroundColor = exports.color.black;
+exports.color.textColor = exports.color.white;
 exports.color.appBarBackground = exports.color.black;
 exports.color.appBarTextColor = exports.color.white;
 exports.color.sideNavBackground = exports.color.charcoal;
