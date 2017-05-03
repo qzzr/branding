82ae85aeafbb83b373fc36d3b205afdd542dc5d9
Jake Richan <jake.richan@gmail.com>
2017-05-03 11:04:47 -0600

    update qzzr, remove others

--- qzzr/index.js
+++ qzzr/index.js
@@ -1,13 +1,14 @@
 exports = module.exports = require('../');
 
 exports.brandName = 'qzzr';
+exports.theme = 'dark';
 exports.color = exports.color || {};
-exports.color.primary = exports.color.violet;
-exports.color.accent = exports.color.green;
-exports.color.backgroundColor = exports.color.azure;
-exports.color.textColor = exports.color.grayBlue;
-exports.color.appBarBackground = exports.color.violet;
+exports.color.primary = exports.color.royal;
+exports.color.accent = exports.color.royal;
+exports.color.backgroundColor = exports.color.black;
+exports.color.textColor = exports.color.white;
+exports.color.appBarBackground = exports.color.black;
 exports.color.appBarTextColor = exports.color.white;
-exports.color.sideNavBackground = exports.color.white;
-exports.color.dashboardBackground = exports.color.azure;
-exports.color.dashboardTextColor = exports.color.gray;
+exports.color.sideNavBackground = exports.color.charcoal;
+exports.color.dashboardBackground = exports.color.black;
+exports.color.dashboardTextColor = exports.color.white;
